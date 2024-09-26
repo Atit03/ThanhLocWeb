5f19663d4d59eae88b1c312dbff38eeb8b68c0c1
Nguyen Thanh Loc <21110897@student.hcmute.edu.vn>
2024-09-26 08:28:01 +0700

    basic structure page

--- fe/thanhlocshop/src/pages/users/homePage/index.js
+++ fe/thanhlocshop/src/pages/users/homePage/index.js
@@ -1,11 +1,6 @@
 import {memo} from "react";
-const homePage = () =>{
-    return (
-        <>
-        <h1>Header</h1>
-        <h1>Body</h1>
-        <h1>Footer</h1>
-        </>
-    );
+const HomePage = () =>{
+    return  <h1>HomePage</h1>;
+
 }
-export default memo(homePage);
+export default memo(HomePage);
